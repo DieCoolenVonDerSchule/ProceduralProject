7d017e8d13e951794ac06cf5af3834dce954fb7e
DieCoolenVonDerSchule <DieCoolenVonDerSchule@gmx.de>
2021-05-09 12:44:00 +0200

    neues default preset

--- Assets/ButtonSetDefault.cs
+++ Assets/ButtonSetDefault.cs
@@ -13,13 +13,13 @@ public class ButtonSetDefault : MonoBehaviour
 
         GameObject.FindGameObjectWithTag("sizex").GetComponent<UnityEngine.UI.InputField>().text = "500";
         GameObject.FindGameObjectWithTag("sizey").GetComponent<UnityEngine.UI.InputField>().text = "500";
-        GameObject.FindGameObjectWithTag("scalex").GetComponent<UnityEngine.UI.InputField>().text = "0,05";
+        GameObject.FindGameObjectWithTag("scalex").GetComponent<UnityEngine.UI.InputField>().text = "0,015";
        
         GameObject.FindGameObjectWithTag("shiftx").GetComponent<UnityEngine.UI.InputField>().text = "0";
         GameObject.FindGameObjectWithTag("shifty").GetComponent<UnityEngine.UI.InputField>().text = "0";
-        GameObject.FindGameObjectWithTag("spreadx").GetComponent<UnityEngine.UI.InputField>().text = "1";
-        GameObject.FindGameObjectWithTag("spready").GetComponent<UnityEngine.UI.InputField>().text = "6,5";
-        GameObject.FindGameObjectWithTag("output").GetComponent<UnityEngine.UI.InputField>().text = "4";
+        GameObject.FindGameObjectWithTag("spreadx").GetComponent<UnityEngine.UI.InputField>().text = "1,3";   //coarse
+        GameObject.FindGameObjectWithTag("spready").GetComponent<UnityEngine.UI.InputField>().text = "6,5";  //contrib
+        GameObject.FindGameObjectWithTag("output").GetComponent<UnityEngine.UI.InputField>().text = "6";
 
         GameObject.FindGameObjectWithTag("maps").GetComponent<UnityEngine.UI.InputField>().text = "5";
         GameObject.FindGameObjectWithTag("speed").GetComponent<UnityEngine.UI.InputField>().text = "1";
